1ce5fa09c8934a9e64f660718b6284ab0c17cd2b
Harold Prentiss <harv.prentiss@gmail.com>
2015-10-30 19:00:33 -0400

    Blackjack Work
    -Added deck factory
    -Added hand and dealing to controller
    -Displaying cards in the view

--- JS/script.js
+++ JS/script.js
@@ -33,6 +33,33 @@ casinoApp.config(function($routeProvider){
 		});
 });
 
+casinoApp.factory('DeckFactory', function(){
+	var suits = [
+		{name:'Hearts', abbr:'H', symbol:'\u2665'},
+		{name:'Clubs', abbr:'C', symbol:'\u2663'},
+		{name:'Diamonds', abbr:'D', symbol:'\u2666'},
+		{name:'Spades', abbr:'S', symbol:'\u2660'}
+	];
+
+	var ranks = [];
+	ranks.push({label:'A', value:1, secondaryValue: 11});
+	ranks.push({label:'K', value:13, secondaryValue: 10});
+	ranks.push({label:'Q', value:12, secondaryValue: 10});
+	ranks.push({label:'J', value:11, secondaryValue: 10});
+	for(rank = 2; rank <= 10; rank+=1){
+		ranks.push({label:rank, value:rank, secondaryValue:rank});
+	}
+
+	var deck = [];
+
+	for(cardSuit = 0; cardSuit < suits.length; cardSuit++){
+		for(cardRank = 0; cardRank < ranks.length; cardRank++){
+			deck.push({suit:suits[cardSuit], rank:ranks[cardRank]});
+		}
+	}
+	return deck;
+});
+
 //Main Controller
 casinoApp.controller('mainController', function($scope){
 
@@ -69,9 +96,59 @@ casinoApp.controller('contactController', function($scope){
 });
 
 //Blackjack Controller
-casinoApp.controller('blackjackController', function($scope){
+casinoApp.controller('blackjackController',  function($scope, DeckFactory){
 
 	$scope.message = 'Talk to me';
 	$scope.pageClass = 'page-blackjack';
+	$scope.Deck = DeckFactory;
+	$scope.deckPosition = 0;
+	$scope.playerCards = [];
+	$scope.dealerCards = [];
+
+	$scope.shuffle = function(){
+		var shuffledDeck = new Array(52);
+
+		// This is a card shuffling algorithm I found online
+		for (var i = 0; i < $scope.Deck.length; i++) {    
+	        var rand = Math.floor(Math.random() * (i + 1));  
+	        shuffledDeck[i] = shuffledDeck[rand];
+	        shuffledDeck[rand] = $scope.Deck[i];
+	    }
+
+	    $scope.Deck = shuffledDeck;
+	};
+
+	$scope.clearCards = function(){
+		$scope.playerCards = [];
+		$scope.dealerCards = [];
+	};
+
+	$scope.dealCard = function(target, dealerTurn){
+		if(target == "p"){
+			if($scope.playerCards.length < 5){
+				$scope.playerCards.push($scope.Deck[$scope.deckPosition]);
+			}
+		}
+		else{
+			if($scope.dealerCards.length < 5){
+				$scope.dealerCards.push($scope.Deck[$scope.deckPosition]);
+			}
+		}
+		$scope.deckPosition++;
+	};
+
+	$scope.dealHand = function(){
+		$scope.shuffle();
+		$scope.clearCards();
+		$scope.deckPosition = 0;
+		$scope.dealCard("p", false);
+		$scope.dealCard("c", false);
+		$scope.dealCard("p", false);
+		$scope.dealCard("c", false);
+	};
+	
+	$scope.dealHand();
+
+	console.log($scope.playerCards);
 
 });
